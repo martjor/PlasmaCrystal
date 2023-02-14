cd838ebc87660f444277d27037295d1c6b72db46
JAMO2001 <jorge_martinez3@baylor.edu>
2023-02-14 10:20:19 -0600

    Modified getVideo func

--- src/pcryGetVid.m
+++ src/pcryGetVid.m
@@ -1,18 +1,29 @@
-function video = getVid(fileName)
-%getVid Extracts the frames from a video file
-%   Detailed explanation goes here
+% File Name:            pcryGetVideo.m
+% Author:               Jorge Augusto Martinez-Ortiz
+% Date Created:         02.13.2023
+% Description:          Function that will open a video file and return 
+%                       an object containing its frames, as well as other
+%                       information about the video such as frame rate.
+function video = pcryGetVid(path,varargin)
+    vid = VideoReader(path);
     
-    vid = VideoReader(fileName);
+    % Read entire video or specific range of frames
+    if nargin == 2
+        frames = read(vid,varargin{2});         
+    elseif nargin == 1
+        frames = read(vid);
+    else
+        error('Invalid number of input arguments');
+    end
 
-    frames = read(vid);         % Read frames
-    frames = frames(:,:,1,:);   % Convert to grayscale
+    frames = frames(:,:,1,:);          % Convert to grayscale
     frames = squeeze(frames);
 
     video.frame = frames;
     video.frameRate = vid.FrameRate;
-    video.numFrames = vid.NumFrames;
+    video.numFrames = size(frames,3);
     video.shape     = [size(frames,1) size(frames,2)];
     
-    fprintf('Read %3i frames @%3i fps',video.numFrames,video.frameRate);
+    fprintf('Read %3i frames @%3i fps\n',video.numFrames,video.frameRate);
 end
 
