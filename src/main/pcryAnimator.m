05143ad9efae689f893d60049217bad7bf086df0
JAMO2001 <jorge_martinez3@baylor.edu>
2023-04-04 23:26:00 -0500

    Implement Animator class

--- src/main/pcryAnimator.m
+++ src/main/pcryAnimator.m
@@ -1,30 +1,98 @@
 classdef pcryAnimator
     properties (Access=public)
-        FileName
         AnimFunc
-    end
-    properties (Access=private)
-        VideoWriter
+        BatchSize = 50;
     end
 
     methods
-        function obj = pcryAnimator(fileName,func)
-            % Initialize variables
-            obj.FileName = fileName; 
+        % Class Constructor
+        function obj = pcryAnimator(func)
+            % Initialize plot function
             obj.AnimFunc = func;
-
-            % Initialize VideoWriter
-            obj.VideoWriter = VideoWriter(obj.FileName);
         end
 
+
         function plotFrame(obj,n)
-            gcf
             obj.AnimFunc(n);
         end
 
-        function print(obj)
-            disp(obj.FileName);
+        function animate(obj,frameVec)
+            % Determine number of frames
+            numFrames = numel(frameVec);
+
+            % Get current figure and axes
+            fig = gcf;
+            gca;
+
+            % Animate
+            i = 1;
+            while (i <= numFrames) && isvalid(fig)
+                cla
+                obj.plotFrame(frameVec(i))
+                drawnow
+                
+                i = i + 1;
+            end
         end
 
+        function recordVideo(obj,frameVec,fileName,varargin)
+            % Determine the number of frames
+            numFrames = numel(frameVec);
+            frameLast = frameVec(end);
+            frameFirst = frameVec(1);
+
+            % Generate struct to store data
+            M(obj.BatchSize) = struct('cdata',[],'colormap',[]);
+
+            % Get current figure and axes
+            fig = gcf;
+            fig.Visible = 'off';
+            gca;
+
+            % Create VideoWriter and open file
+            video = VideoWriter(fileName);
+            video.Quality = 100;
+            open(video);
+
+            % Animate and record
+            i = 1; j = 0;
+            while (i <= numFrames) && isvalid(fig)
+                % Generate plot
+                cla
+                obj.plotFrame(frameVec(i))
+                drawnow
+
+                % Store frame
+                cdata = print('-RGBImage','-r300');
+                M(j+1) = im2frame(cdata);
+
+                % Update
+                percent = i/numFrames * 100;
+                fprintf("Generating frame %i (%.1f%%)\n",frameVec(i),percent);
+
+                % Update batch index
+                j = j+1;
+                if (mod(j,obj.BatchSize) == 0) || (j == numFrames)
+                    numFramesGenerated = j;
+                    j = 0;
+                end
+    
+                % Write batch
+                if (j==0) && (i~=1) 
+                    fprintf("\tWriting...\n")
+                    writeVideo(video,M(1:numFramesGenerated));
+                    fprintf("\tDone.\n")
+                end
+                i = i + 1;
+            end
+
+            % Close video
+            close(video);
+        end
+
+        % SETTERS
+        function obj = set.BatchSize(obj,n)
+            obj.BatchSize = n;
+        end
     end
 end
--- src/test/testAnimator.m
+++ src/test/testAnimator.m
@@ -1,3 +1,32 @@
-T = pcryReadTable('../../data/20210803_091919_01.csv','fiji');
-myFunc = @(x) sin(x);
-Anim = pcryAnimator('video.avi',myFunc)
+path = '/Users/jorgeaugustomartinezortiz/Library/CloudStorage/OneDrive-BaylorUniversity/CASPER/projects/plasma_crystal/crystal_analysis/torsions_datasets/laser_pulse_20221006_crop_0_999.csv';
+T = pcryReadTable(path);
+
+% Define function and initialize animator
+numFrames = unique(T.frame)
+myFunc = @(frameNum) plotFrame(T,frameNum);
+Anim = pcryAnimator(myFunc)
+
+figure
+ax = axes;
+xlim([min(T.x) max(T.x)])
+ylim([min(T.y) max(T.y)])
+colormap copper
+clim([0 1])
+
+%Anim.animate(1:999)
+Anim.recordVideo(1:999,'localBondOrder.avi')
+function plotFrame(T,frameNum)
+    % Get current frame
+    frame = pcryGetFrame(T,frameNum);
+    x = frame.x;
+    y = frame.y;
+
+    % Calculate the Delaunay triangulation
+    DT = delaunay([x y]);
+    
+    % Calculate the local bond order parameter for n=6 
+    g6 = pcryLocalBondOrder(DT,[x y],6);
+
+    pcryVoronoi(x,y,g6);
+    title(sprintf("Frame %i",frameNum));
+end
